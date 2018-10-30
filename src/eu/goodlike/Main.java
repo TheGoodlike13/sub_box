516eb8c60e0cb2fd5630ff0641b16e2a88b083c6
TheGoodlike13 <henrisssss@hotmail.com>
2018-10-30 18:12:28 +0200

    Initial commit

---
+++ src/eu/goodlike/Main.java
@@ -0,0 +1,9 @@
+package eu.goodlike;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("What are you doing, gradle?");
+    }
+
+}
---
+++ test/eu/goodlike/MainTest.java
@@ -0,0 +1,14 @@
+package eu.goodlike;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.fail;
+
+public class MainTest {
+
+    @Test
+    public void test() {
+        fail("yay!");
+    }
+
+}
