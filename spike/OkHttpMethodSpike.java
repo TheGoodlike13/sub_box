a21caba853fbb081979e209801f45f4b412bfd1d
TheGoodlike13 <henrisssss@hotmail.com>
2018-12-18 17:35:15 +0200

    S1P8 spike for OkHttp HTTP method handling

---
+++ spike/OkHttpMethodSpike.java
@@ -0,0 +1,27 @@
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+import okhttp3.internal.Util;
+import okhttp3.internal.http.StatusLine;
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+public final class OkHttpMethodSpike {
+
+    public static void main(String... args) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder().url("https://www.google.lt/").method(" ", Util.EMPTY_REQUEST).build();
+        Response response = client.newCall(request).execute();
+        System.out.println(StatusLine.get(response));
+        ResponseBody body = response.body();
+        if (body == null)
+            System.out.println("No response body.");
+        else
+            System.out.println(IOUtils.toString(body.byteStream(), Charset.forName("UTF-8")));
+    }
+
+}
