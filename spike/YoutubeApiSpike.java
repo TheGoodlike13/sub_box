52c51341397fa3aec2d18d1745d57eab06eae158
Henrikas <henrisssss@hotmail.com>
2018-12-23 13:47:32 +0200

    S1P9 tested out OkHttpTransport using YoutubeApiSpike

--- spike/YoutubeApiSpike.java
+++ spike/YoutubeApiSpike.java
@@ -1,15 +1,16 @@
-import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.youtube.YouTube;
 import com.google.api.services.youtube.model.SearchListResponse;
 import com.google.api.services.youtube.model.SearchResult;
+import eu.goodlike.sub.box.http.OkHttpTransport;
+import okhttp3.OkHttpClient;
 
 import java.io.IOException;
 
 public final class YoutubeApiSpike {
 
   public static void main(String... args) throws IOException {
-    YouTube youTube = new YouTube.Builder(new NetHttpTransport(), new JacksonFactory(), null)
+    YouTube youTube = new YouTube.Builder(new OkHttpTransport(new OkHttpClient()), new JacksonFactory(), null)
         .setApplicationName("sub_box")
         .build();
 
