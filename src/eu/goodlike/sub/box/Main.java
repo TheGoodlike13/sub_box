ddf396364a02459efde99dd44595afdf41b10599
TheGoodlike13 <henrisssss@hotmail.com>
2019-03-18 18:41:57 +0200

    S1P10 Implemented basic Main mechanism for channel search

--- src/eu/goodlike/sub/box/Main.java
+++ src/eu/goodlike/sub/box/Main.java
@@ -1,9 +1,66 @@
 package eu.goodlike.sub.box;
 
-public class Main {
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.youtube.YouTube;
+import eu.goodlike.sub.box.http.OkHttpTransport;
+import eu.goodlike.sub.box.search.Result;
+import eu.goodlike.sub.box.search.Search;
+import eu.goodlike.sub.box.youtube.YoutubeChannelSearch;
+import okhttp3.OkHttpClient;
+import org.apache.commons.lang3.StringUtils;
 
-  public static void main(String[] args) {
-    System.out.print("What are you doing, gradle?");
+import java.io.IOException;
+import java.util.Scanner;
+
+public final class Main implements AutoCloseable {
+
+  public static void main(String... args) throws IOException {
+    try (Main main = new Main()) {
+      main.run();
+    }
+
+    System.out.println("Bye!");
+  }
+
+  public void run() throws IOException {
+    System.out.println("Channel search");
+
+    String input;
+    while ((input = readInput()) != null)
+      performSearch(input);
+  }
+
+  @Override
+  public void close() throws IOException {
+    transport.shutdown();
+  }
+
+  public Main() {
+    this.transport = new OkHttpTransport(new OkHttpClient());
+
+    YouTube youtube = new YouTube.Builder(transport, new JacksonFactory(), null)
+        .setApplicationName("sub_box")
+        .build();
+    this.channelSearch = new YoutubeChannelSearch(youtube);
+  }
+
+  private final HttpTransport transport;
+  private final Search channelSearch;
+
+  private void performSearch(String input) throws IOException {
+    channelSearch.doSearch(input, 1).forEach(this::printSearchResult);
+  }
+
+  private void printSearchResult(Result result) {
+    System.out.println("Found channel: " + result.getTitle() + " @" + result.getUrl());
+  }
+
+  private static final Scanner INPUT_READER = new Scanner(System.in);
+
+  private static String readInput() {
+    System.out.print("Search query: ");
+    return StringUtils.trimToNull(INPUT_READER.nextLine());
   }
 
 }
