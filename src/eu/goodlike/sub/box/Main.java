11fc3f5925502c3405ab7435b60b4294abd57e4f
TheGoodlike13 <henrisssss@hotmail.com>
2019-03-18 18:43:29 +0200

    S1P10 Main now uses OkHttp interceptor for YouTube API key

--- src/eu/goodlike/sub/box/Main.java
+++ src/eu/goodlike/sub/box/Main.java
@@ -4,6 +4,7 @@ import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.youtube.YouTube;
 import eu.goodlike.sub.box.http.OkHttpTransport;
+import eu.goodlike.sub.box.http.YoutubeApiKeyProvider;
 import eu.goodlike.sub.box.search.Result;
 import eu.goodlike.sub.box.search.Search;
 import eu.goodlike.sub.box.youtube.YoutubeChannelSearch;
@@ -37,7 +38,10 @@ public final class Main implements AutoCloseable {
   }
 
   public Main() {
-    this.transport = new OkHttpTransport(new OkHttpClient());
+    OkHttpClient client = new OkHttpClient.Builder()
+        .addInterceptor(new YoutubeApiKeyProvider(PUBLIC_API_KEY))
+        .build();
+    this.transport = new OkHttpTransport(client);
 
     YouTube youtube = new YouTube.Builder(transport, new JacksonFactory(), null)
         .setApplicationName("sub_box")
@@ -57,6 +61,7 @@ public final class Main implements AutoCloseable {
   }
 
   private static final Scanner INPUT_READER = new Scanner(System.in);
+  private static final String PUBLIC_API_KEY = "AIzaSyC7Z0dhCTFFR_0Gt4YpuFjIlmEPFuvqpg8";
 
   private static String readInput() {
     System.out.print("Search query: ");
